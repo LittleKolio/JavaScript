b2e409a11e33ffb800fb20f7fdb0dc5070ea33fe
LittleKolio <kulio_kolef@abv.bg>
2016-06-19 19:10:58 +0300

    Update JS.Lab.06.Largest3Numbers.js

--- JS.Lab.06.Largest3Numbers.js
+++ JS.Lab.06.Largest3Numbers.js
@@ -1,1 +1,8 @@
-dadada
+function max3 (arr) {
+	let nums = arr.map (Number);
+	nums.sort ((a, b) => b - a);
+	let count = Math.min (3, nums.length);
+	for (let i = 0; i < count; i++) {
+		console.log (nums [i]);
+	}
+}
